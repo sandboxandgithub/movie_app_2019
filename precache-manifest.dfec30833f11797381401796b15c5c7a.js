7f9feba19ca7294604109b50772ade13af4a50ed
bocho <bochoej@gmail.com>
2020-10-07 04:47:32 +0900

    Updates

---
+++ precache-manifest.dfec30833f11797381401796b15c5c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069e489c0ab5f921229a4dd3bd935e03",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e007572d02e7638edd89",
+    "url": "/movie_app_2019/static/css/main.1aa15996.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e007572d02e7638edd89",
+    "url": "/movie_app_2019/static/js/main.9e1afb74.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
